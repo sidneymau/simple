6b110a4986bfa66afd97b86804175bb3988fc69f
Sidney Mau <sidneymau@gmail.com>
2020-05-08 13:32:38 -0500

    Moving functions around

--- simple/survey.py
+++ simple/survey.py
@@ -13,6 +13,9 @@ import ugali.utils.healpix
 #----------------------------------------------------------
 
 class Survey():
+    """
+    Class to handle survey-specific parameters.
+    """
     def __init__(self, iterable=(), **kwargs):
         self.__dict__.update(iterable, **kwargs)
 
@@ -28,7 +31,22 @@ class Survey():
                      self.mag_err_1, self.mag_err_2,
                      self.mag_dered_1, self.mag_dered_2]
 
+    def get_fracdet(self):
+        """
+        Load-in and return the fracdet map if it exists.
+        """
+        if (self.fracdet is not None) and (self.fracdet.lower().strip() != 'none') and (self.fracdet != ''):
+            print('Reading fracdet map {} ...').format(self.fracdet)
+            fracdet = ugali.utils.healpix.read_map(self.fracdet)
+        else:
+            print('No fracdet map specified ...')
+            fracdet = None
+        return(fracdet)
+
     def get_data(self, pixels):
+        """
+        Load-in and return data for a list of healpixels as a numpy array.
+        """
         data_array = []
         for pixel in pixels:
             inlist = glob.glob('{}/*_{:05d}.fits'.format(self.datadir, pixel))
@@ -39,11 +57,3 @@ class Survey():
         data = np.concatenate(data_array)
         return(data)
 
-    def get_fracdet(self):
-        if (self.fracdet is not None) and (self.fracdet.lower().strip() != 'none') and (self.fracdet != ''):
-            print('Reading fracdet map {} ...').format(self.fracdet)
-            fracdet = ugali.utils.healpix.read_map(self.fracdet)
-        else:
-            print('No fracdet map specified ...')
-            fracdet = None
-        return(fracdet)

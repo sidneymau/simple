5c58fff8662c4af4760f2f005e9d1e03e629e08a
Sidney Mau <sidneymau@gmail.com>
2020-05-08 13:13:13 -0500

    Clean-up and add methods

--- simple/survey.py
+++ simple/survey.py
@@ -3,33 +3,18 @@
 """
 __author__ = "Sidney Mau"
 
+import glob
+import os
+
+import numpy as np
+
+import ugali.utils.healpix
+
 #----------------------------------------------------------
 
 class Survey():
     def __init__(self, iterable=(), **kwargs):
         self.__dict__.update(iterable, **kwargs)
-
-        #self.survey = cfg['survey']
-        #self.nside = cfg['nside']
-        #self.mag_max = cfg['mag_max']
-
-        #self.datadir = cfg['datadir']
-
-        #self.basis_1 = cfg['basis_1']
-        #self.basis_2 = cfg['basis_2']
-
-        #self.band_1 = cfg['band_1']
-        #self.band_2 = cfg['band_2']
-
-        #self.mag = cfg['mag']
-        #self.mag_err = cfg['mag_err']
-        #self.mag_dered = cfg['mag_dered']
-
-        #self.fracdet = cfg['fracdet']
-
-        #self.mode = cfg['mode']
-        #self.sim_population = cfg['sim_population']
-        #self.sim_dir = cfg['sim_dir']
 
         self.mag_1 = self.mag.format(self.band_1.upper())
         self.mag_2 = self.mag.format(self.band_2.upper())
@@ -42,3 +27,23 @@ class Survey():
                      self.mag_1, self.mag_2,
                      self.mag_err_1, self.mag_err_2,
                      self.mag_dered_1, self.mag_dered_2]
+
+    def get_data(self, pixels):
+        data_array = []
+        for pixel in pixels:
+            inlist = glob.glob('{}/*_{:05d}.fits'.format(self.datadir, pixel))
+            for infile in inlist:
+                if not os.path.exists(infile):
+                    continue
+                data_array.append(fits.read(infile, columns=self.cols))
+        data = np.concatenate(data_array)
+        return(data)
+
+    def get_fracdet(self):
+        if (self.fracdet is not None) and (self.fracdet.lower().strip() != 'none') and (self.fracdet != ''):
+            print('Reading fracdet map {} ...').format(self.fracdet)
+            fracdet = ugali.utils.healpix.read_map(self.fracdet)
+        else:
+            print('No fracdet map specified ...')
+            fracdet = None
+        return(fracdet)
